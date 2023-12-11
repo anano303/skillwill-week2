b08fffdd0c8f2afcbe09ffbd45a04f8476ac5789
anano303 <124179773+anano303@users.noreply.github.com>
2023-12-11 18:48:28 +0400

    add

---
+++ style.js
@@ -0,0 +1,36 @@
+// function replaceValues(inputString, valueToReplace, valueToReplaceWith) {
+//   return inputString.replace(
+//     new RegExp(valueToReplace, "g"),
+//     valueToReplaceWith
+//   );
+// }
+
+// let originalString = "Hello, World! Hello, Universe!";
+// let replacementValue = "Hello";
+// let newValue = "Hi";
+
+// let result = replaceValues(originalString, replacementValue, newValue);
+// console.log(result); ----------ვერ ვაკეთებ ამას სხვანაირად ვერ გავიგე ის გრძლად გაკეთებული
+
+// function capitalizeWords(sentence) {
+//   return sentence
+//     .split(" ")
+//     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join(" ");
+// }
+
+// let inputSentence = "this is a sample sentence.";
+// let result = capitalizeWords(inputSentence);
+// console.log(result);
+
+// function sortByAge(users) {
+//   return users.slice().sort((a, b) => a.age - b.age);
+// }
+
+// let users = [
+//   { name: "Lasha", age: 30 },
+//   { name: "Saba", age: 20 },
+// ];
+
+// let sortedUsers = sortByAge(users);
+// console.log(sortedUsers);
